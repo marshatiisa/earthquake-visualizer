4532981745a0a72f25a12236114943f03271dd43
Marsha <marshatiisa35@gmail.com>
2023-08-09 15:53:32 -0400

    add helper functions for times

--- main.py
+++ main.py
@@ -8,7 +8,19 @@ def extract_subarea(place):
     return place[0]
 
 def extract_area(place):
-    return place[1]
+    return place[-1]
+
+def extract_date(time):
+    return str(time).split(' ')[0]
+
+def extract_weekday(time):
+    date = extract_date(time)
+    return date + ' - ' + str(time.weekday())
+
+def extract_hour(time):
+    t = str(time).split(' ')
+    return t[0] + ' - ' + t[1].split(':')[0]
+
 # Fetch data and clean it up
 def fetch_eq_data(period='daily', region="Worldwide", min_mag=1):
     url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/{}.csv'
@@ -45,5 +57,11 @@ def fetch_eq_data(period='daily', region="Worldwide", min_mag=1):
         center_long = df_earthquake[df_earthquake['mag'] == max_mag]['longitude'].values[0]
     else:
         center_lat, center_long = [54,15]
+
+    #Set columns for animation frames
+    #weekdays, dates and hours
+    if period == 'weekly':
+        animation_frame_col = 'weekday'
+        df_earthquake[animation_frame_col] = df_earthquake['time']# to complete later
 
 # Create visualizer
